5af2415e0ec9ede3d0f84ff054959c7d8a12bd95
Jordan <Jordanabid@gmail.com>
2022-10-13 10:52:02 +0200

    DemoCrud

--- HopitalMaven/src/test/java/test/TestJPA.java
+++ HopitalMaven/src/test/java/test/TestJPA.java
@@ -5,9 +5,25 @@ import javax.persistence.Persistence;
 
 public class TestJPA {
 
+	
+	
+	///insert / update Patient (save)
+	//findById Patient
+	//deleteById Patient
+	//findAll Patient
+	//FindAll Visite
+	//FindAllVisitesByIdPatient
+	
+	
+	
+	
 	public static void main(String[] args) {
 		
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("hopital");
+		
+		
+		
+		
 		emf.close();
 
 	}
