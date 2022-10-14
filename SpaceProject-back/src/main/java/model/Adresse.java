0e6bbb0101b7318caf1513eed4d57137f13b00ea
Tapotix <gaulmatthieu@gmail.com>
2022-10-14 11:31:06 +0200

    AddAdresse

--- SpaceProject-back/src/main/java/model/Adresse.java
+++ SpaceProject-back/src/main/java/model/Adresse.java
@@ -2,13 +2,30 @@ package model;
 
 import java.io.Serializable;
 
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+
+@Embeddable
 public class Adresse implements Serializable{
-	private String numero;
-	private String voie;
-	private String ville;
-	private String cp;
-	private Pays pays;
 	
+	@Column(nullable = false,columnDefinition = "VARCHAR(8) default 'NULL'")
+	private String numero;
+	
+	@Column(nullable = false,columnDefinition = "VARCHAR(50) default 'NULL'")
+	private String voie;
+	
+	@Column(nullable = false,columnDefinition = "VARCHAR(50) default 'NULL'")
+	private String ville;
+	
+	@Column(nullable = false,columnDefinition = "VARCHAR(10) default 'NULL'")
+	private String cp;
+	
+	@Column(nullable = false,columnDefinition = "INT(8) default 'NULL'")
+	private Pays pays;
+
+	
+	public Adresse() {
+	}
 	
 	public Adresse(String numero, String voie, String ville, String cp, Pays pays) {
 		this.numero = numero;
