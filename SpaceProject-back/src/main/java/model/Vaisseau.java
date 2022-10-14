2f492506003c8a625afc2ca7b7e0440d3b285a8b
hinda <hind.elhassouni00@gmail.com>
2022-10-14 11:44:39 +0200

    Update Classe  vaisseau

--- SpaceProject-back/src/main/java/model/Vaisseau.java
+++ SpaceProject-back/src/main/java/model/Vaisseau.java
@@ -2,15 +2,38 @@ package model;
 
 import java.io.Serializable;
 import java.time.LocalDate;
+import java.util.List;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="vaisseau")
 public class Vaisseau implements Serializable {
-	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
+	
+   @Column(columnDefinition = "varchar(25) ", nullable = false)
 	private String nom;
+
+	
 	private int capacite;
+	@Column(name="date_service",columnDefinition = "date ", nullable = false)
 	private LocalDate miseEnService;
+	@Column(columnDefinition = " tinyint(1)", nullable = false)
 	private boolean fonctionnel;
+	
+	@Column(name="type_vaisseau",columnDefinition = "ENUM('Fusee(20)','Navette(28)','Sonde(35)')")
 	private TypeVaisseau typeVaisseau ;
+	@OneToMany(mappedBy = "vaisseau")
+	private List<Mission> missions;
+	
 	
 	public Vaisseau(String nom, int capacite, LocalDate miseEnService, boolean fonctionnel,TypeVaisseau typeVaisseau) {
 		this.nom = nom;
@@ -31,6 +54,10 @@ public class Vaisseau implements Serializable {
 	
 	
 	
+	public Vaisseau() {
+		super();
+	}
+
 	//GETTERS
 	public String getNom() {
 		return nom;
