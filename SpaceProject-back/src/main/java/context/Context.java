883314840a3942131aa2da0035c77bad47e51c7e
Maxime Ferrand <ferrand.max12@gmail.com>
2022-10-14 11:24:29 +0200

    update class context

--- SpaceProject-back/src/main/java/context/Context.java
+++ SpaceProject-back/src/main/java/context/Context.java
@@ -3,17 +3,18 @@ package context;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
+import dao.DAOVaisseau;
+
 public class Context {
 
 	private EntityManagerFactory emf = Persistence.createEntityManagerFactory("space");
 	
+	private DAOVaisseau daoVaisseau = new DAOVaisseau();	
+	
 	private static Context singleton=null;
 
-
-	
 	private Context() {}
 	
-
 	public static Context getSingleton() {
 	
 		if(singleton==null) 
@@ -23,13 +24,6 @@ public class Context {
 		return singleton;
 	}
 
-
-
-
-
-
-
-
 	public EntityManagerFactory getEmf() {
 		return emf;
 	}
@@ -37,6 +31,10 @@ public class Context {
 
 	public void setEmf(EntityManagerFactory emf) {
 		this.emf = emf;
+	}
+
+	public static void setSingleton(Context singleton) {
+		Context.singleton = singleton;
 	}
 
 
