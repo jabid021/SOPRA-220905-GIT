81d3387c5b808716a29063d94db1dba90f5e3d67
olivier <o.gozlan@ajc-ingenierie.fr>
2022-11-08 09:34:35 +0100

    support spring boot et security

--- olivier/SPRING-MVC/eshop-web/src/main/java/ajc/sopra/eshop/restcontroller/ClientRestController.java
+++ olivier/SPRING-MVC/eshop-web/src/main/java/ajc/sopra/eshop/restcontroller/ClientRestController.java
@@ -1,14 +1,19 @@
 package ajc.sopra.eshop.restcontroller;
 
+import java.lang.reflect.Field;
+import java.time.LocalDate;
 import java.util.List;
+import java.util.Map;
 
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.ReflectionUtils;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -18,6 +23,8 @@ import org.springframework.web.server.ResponseStatusException;
 
 import com.fasterxml.jackson.annotation.JsonView;
 
+import ajc.sopra.eshop.model.Adresse;
+import ajc.sopra.eshop.model.Client;
 import ajc.sopra.eshop.model.Client;
 import ajc.sopra.eshop.model.JsonViews;
 import ajc.sopra.eshop.service.ClientService;
@@ -48,5 +55,28 @@ public class ClientRestController {
 	@GetMapping("")
 	public List<Client> findAll() {
 		return clientSrv.findAll();
+	}
+	
+	@PatchMapping("/{id}")
+	@JsonView(JsonViews.Common.class)
+	public Client patch(@RequestBody Map<String, Object> fields, @PathVariable Integer id) {
+		Client client = clientSrv.findById(id);
+		fields.forEach((k, v) -> {
+			if (k.equals("adresse")) {
+				Map<String, Object> mapAdresse = (Map<String, Object>) v;
+				mapAdresse.forEach((kAdresse, vAdresse) -> {
+					Field fieldAdresse = ReflectionUtils.findField(Adresse.class, kAdresse);
+					ReflectionUtils.makeAccessible(fieldAdresse);
+					ReflectionUtils.setField(fieldAdresse, client.getAdresse(), vAdresse);
+				});
+			}else if(k.equals("naissance")) {
+				client.setNaissance(LocalDate.parse(v.toString()));
+			} else {
+				Field field = ReflectionUtils.findField(Client.class, k);
+				ReflectionUtils.makeAccessible(field);
+				ReflectionUtils.setField(field, client, v);
+			}
+		});
+		return clientSrv.save(client);
 	}
 }
